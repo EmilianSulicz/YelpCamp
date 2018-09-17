9772c7d696d705554a48b80a8147871bf0dbe3d4
Emilian <emilian321@gmail.com>
2018-09-17 14:30:46 +0000

    initial deploy

---
+++ app.js
@@ -0,0 +1,60 @@
+//in other terminal "cd" -> "./mongod"
+//npm init
+//npm install express mongoose body-parser passport passport-local passport-local-mongoose express-session ejs method-override --save
+//npm install --save connect-flash moment
+var express         = require("express"),
+    app             = express(),
+    bodyParser      = require("body-parser"),
+    mongoose        = require("mongoose"),
+    Campground      = require("./models/campground"),
+    Comment         = require("./models/comment"),
+    methodOverride  = require("method-override"),
+    seedDB          = require("./seeds"),
+    passport        = require("passport"),
+    flash           = require("connect-flash"),
+    LocalStrategy   = require("passport-local"),
+    User            = require("./models/user");
+
+//Requiring routes    
+var commentRoutes       = require("./routes/comments"),
+    campgroundRoutes    = require("./routes/campgrounds"),
+    indexRoutes         = require("./routes/index");
+
+//seed the database
+// seedDB(); 
+
+mongoose.connect("mongodb://localhost/yelp_camp");
+app.use(bodyParser.urlencoded({extended: true}));
+app.set("view engine", "ejs");
+app.use(express.static(__dirname + "/public"));
+app.use(methodOverride("_method"));
+app.use(flash());
+app.locals.moment = require('moment');
+//PASSPORT CONFIGURATION
+app.use(require("express-session")({
+    secret:"Once upon a time",
+    resave: false,
+    saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+app.use(function(req, res, next){
+    res.locals.currentUser = req.user;
+    res.locals.error = req.flash("error");
+    res.locals.success = req.flash("success");
+    next();
+});
+
+app.use(indexRoutes);
+app.use("/campgrounds", campgroundRoutes);
+app.use("/campgrounds/:id/comments", commentRoutes);
+
+
+
+app.listen(process.env.PORT, process.env.IP, function(){
+   console.log("The YelpCamp server has started!");
+});
